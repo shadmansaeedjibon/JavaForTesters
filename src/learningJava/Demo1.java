4b59ca812a72923a29c7916c170aa87bcdcafb27
shadmansaeedjibon <jibonrootnext@gmail.com>
2023-04-21 06:29:56 +0600

    project added

---
+++ src/learningJava/Demo1.java
@@ -0,0 +1,16 @@
+package learningJava;
+
+public class Demo1 //classname
+{
+   public static void main(String[] args) {
+	
+	  Demo1 de= new Demo1(); // new Demo1 is the object of particular class
+	  // de= instance variable
+	  new Demo1();
+	  new Demo1();
+	  new Demo1();
+	  new Demo1();
+	  
+	   
+  }
+}
